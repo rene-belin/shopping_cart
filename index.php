28f7ee1e0f6ca0df935ed43c2c7f37ce9e228ade
rene-belin <rene.b@gmx.fr>
2024-05-24 20:46:39 +0200

    mise en place de l'include header dans l'index

--- index.php
+++ index.php
@@ -13,23 +13,8 @@
 </head>
 
 <body>
-    <!-- header-->
-    <header class="header">
-        <div class="header_body">
-            <a href="index.php" class="logo">Technobling</a>
-            <!-- nav-->
-            <nav>
-                <a href="">Ajouter des produits</a>
-                <a href="">Voir les produits</a>
-                <a href="">Achetez-le</a>
-            </nav>
-
-            <!-- shopping cart icon-->
-            <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
-            <div id="menu-btn" class="fas fa-bars"></div>
-        </div>
-
-    </header>
+    <!-- include header-->
+    <?php include 'header.php'?>
 
     <!-- form section-->
     <div class="container">
